2bd5da133c62a7a4e6d7f066f5f0e089acc0297e
VedaV <valivetiveda@gmail.com>
2020-01-25 12:31:13 -0800

    Add files via upload

---
+++ zero/zero/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  zero
+//
+//  Created by Veda Valiveti on 1/25/20.
+//  Copyright © 2020 Veda Valiveti. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ zero/zero/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  zero
+//
+//  Created by Veda Valiveti on 1/25/20.
+//  Copyright © 2020 Veda Valiveti. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
