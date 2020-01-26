241fbe54e8e44a121b7c3c3974ec3a7256cfd4e9
VedaV <valivetiveda@gmail.com>
2020-01-25 21:28:26 -0800

    water page

--- zero/zero/FirstViewController.swift
+++ zero/zero/FirstViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-private var mood1 = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("bob")
+        
     }
     
     
@@ -24,11 +24,7 @@ private var mood1 = 0
         // Dispose of any resources that can be rcreated.
     }
 
-    @IBAction func m1(_ sender: UIButton) {
-        mood1 += 1;
-        print("x")
-    }
-    
+ 
     
     
     
